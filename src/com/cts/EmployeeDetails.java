ecc4551407878d4eaf21e1f035be08c75611c592
Hari120p <haripriyabe1996@gmail.com>
2022-05-15 20:31:02 +0530

    Task Completed

---
+++ src/com/cts/EmployeeDetails.java
@@ -0,0 +1,20 @@
+package com.cts;
+
+public class EmployeeDetails {
+	private void sub() {
+		System.out.println("sub");
+
+	}
+	private void add() {
+		System.out.println("add");
+
+	}
+	private void div() {
+		System.out.println("div");
+
+	}
+	private void mul() {
+	System.out.println("mul");
+	}
+
+}
